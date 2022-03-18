0600f2aab137b517ac9518137924caee189788ff
Arya kesarwani <aryakesharwani1234@gmail.com>
2022-03-18 13:53:16 +0530

    :sparkles: updated backend

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,18 +1,16 @@
-const fs = require("fs");
-const path = require("path");
-const main = (req, res) => {
-  const values = req.body;
-  fs.writeFile(
-    path.resolve("./data/input.json"),
-    JSON.stringify(values),
-    function (err) {
-      if (err) throw err;
-      console.log("complete");
+const shell = require("shelljs");
+const main2 = (req, res) => {
+  shell.exec(
+    "~/Desktop/test/real_sih/venv/bin/python3 ~/Desktop/test/real_sih/dep/view_images.py",
+    function (code, output) {
+      console.log("Exit code:", code);
+      // console.log("Program output:", output);
     }
+    // { silent: true }
   );
-  res.send({ message: "wrote the data" });
+  res.send({ message: "working" });
 };
 
 module.exports = {
-  main,
+  main2,
 };
--- public/js/backend.js
+++ public/js/backend.js
@@ -1,35 +1,21 @@
-const pressureInput = document.getElementById("pressureInput");
-const latitudeInput = document.getElementById("latitudeInput");
-const longitudeInput = document.getElementById("longitudeInput");
-const monthInput = document.getElementById("monthInput");
 const generateButton = document.getElementById("generateButton");
+const valuesOutput = document.getElementById("valuesOutput");
+const imageViewer = document.getElementById("imageViewer");
 
 generateButton.addEventListener("click", async () => {
-  const pressure = Number(pressureInput.value);
-  const latitude = Number(latitudeInput.value);
-  const longitude = Number(longitudeInput.value);
-  const month = Number(monthInput.value);
-  if (!(latitude >= -90 && latitude <= 90))
-    return alert("wrong latitude entered");
-  if (!(longitude >= -180 && longitude <= 180))
-    return alert("wrong longitude entered");
-  if (!(month >= 1 && month <= 12)) return alert("wrong month entered");
   const response = await fetch(`/api/sendData`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      pressure,
-      latitude,
-      longitude,
-      month,
-    }),
   });
   const result = await response.json();
   console.log(result);
+  location.reload();
 });
 
-// window.addEventListener("DOMContentLoaded", (event) => {
-//   console.log("loaded");
-// });
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const response = await fetch("/data/labels.json");
+  const { image_number, speed } = await response.json();
+  valuesOutput.innerHTML = `image number: ${image_number}, Speed(Knots) : ${speed}`;
+});
